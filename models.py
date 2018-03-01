054ae4ebc2a74d33cff63523c9da5100ecbd91ab
maplethorpej <jeradmaplethorpe@gmail.com>
2018-03-01 11:58:51 -0600

    deploy logic

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Building:
     seconds_of_operation = 28800
     elevator_count = 4
@@ -24,13 +27,14 @@ class Building:
         elevator.queue_trip(person.destination)
 
     def find_nearest_elevator(self, location, destination):
-        ideal_elevator = [rec for rec in self.elevators if rec.location == location and not rec.in_transit]
+        ideal_elevator = [rec for rec in self.elevators if
+                          rec.location == location and not rec.in_transit and not rec.service_required]
         if ideal_elevator:
             # if an ideal elevator is found, return the first result
             return ideal_elevator[0]
 
         # rank elevators by distance from location and sort
-        by_rank = [(rec, rec.transit_distance(destination)) for rec in self.elevators]
+        by_rank = [(rec, rec.transit_distance(destination)) for rec in self.elevators if not rec.service_required]
         by_rank.sort(key=lambda rec: rec[1])
 
         return by_rank[0][0]
@@ -52,7 +56,6 @@ class Elevator:
 
     floor_queue = []
     trip_queue = []
-    queue_idx = 0
 
     def __init__(self, uid):
         self.id = uid
@@ -82,6 +85,29 @@ class Elevator:
         init_floor = self.destination if self.in_transit else self.location
         self.trip_queue.append([x for x in range(init_floor, destination)])
         self.floor_queue.extend([x for x in range(init_floor, destination)])
+
+    def deploy(self):
+        self.destination = self.trip_queue[self.trip_count][:-1]
+        self.in_transit = True
+        
+        for floor in self.trip_queue[self.trip_count]:
+            time.sleep(1)
+            self.location = floor
+            self.floors_passed += 1
+            print(self.location)
+
+            if self.destination == floor:
+                self.arrival()
+
+        self.trip_count += 1
+
+    def arrival(self):
+        self.doors_open = True
+        time.sleep(5)
+        self.doors_open = False
+
+        if len(self.trip_queue) > self.trip_count:
+            self.deploy()
 
 
 class Person:
